68ddf4f0514fe3ce791e5bec7e545c1d172c6a25
thomasberrens <48439074+thomasberrens@users.noreply.github.com>
2021-06-16 20:47:35 +0200

    adding cutscene

--- Assets/Scripts/Cutscene/Cutscene.cs
+++ Assets/Scripts/Cutscene/Cutscene.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 
 public class Cutscene : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    private VideoPlayer _videoPlayer;
+    void Awake()
     {
-        
+        _videoPlayer = GetComponent<VideoPlayer>();
+        _videoPlayer.Play();
+        _videoPlayer.loopPointReached += CheckOver;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    
+    private void CheckOver(UnityEngine.Video.VideoPlayer vp)
+    {
+        SceneManager.LoadScene("Room1");//the scene that you want to load after the video has ended.
     }
 }
